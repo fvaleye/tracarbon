c1b51e72d83768f2a02835fb29f8432981a98f0b
fvaleye <florian.valeye@gmail.com>
2022-10-03 18:53:25 +0200

    Add JSONExporter in the exporters

---
+++ tests/exporters/test_json_exporter.py
@@ -0,0 +1,74 @@
+from datetime import datetime
+
+import psutil
+import ujson
+
+from tracarbon import Country, HardwareInfo
+from tracarbon.exporters import JSONExporter, Metric
+
+
+def test_json_exporter_should_write_well_formatted_metrics_in_json_file(mocker, tmpdir):
+    mock = mocker.patch("tracarbon.exporters.json_exporter.datetime")
+    fixed_timestamp = datetime(2021, 12, 21)
+    mock.utcnow.return_value = fixed_timestamp
+    test_json_file = tmpdir.mkdir("data").join("test.json")
+    interval_in_seconds = 1
+    cpu_value = "5.0"
+    memory_value = "70"
+    mock_memory_value = ["0", "0", memory_value]
+    mocker.patch.object(psutil, "cpu_percent", return_value=cpu_value)
+    mocker.patch.object(psutil, "virtual_memory", return_value=mock_memory_value)
+    expected = [
+        {
+            "timestamp": str(fixed_timestamp),
+            "metric_name": "test_metric_1",
+            "metric_value": "70",
+            "metric_tags": ["test_tags"],
+        },
+        {
+            "timestamp": str(fixed_timestamp),
+            "metric_name": "test_metric_2",
+            "metric_value": "5.0",
+            "metric_tags": ["test_tags"],
+        },
+        {
+            "timestamp": str(fixed_timestamp),
+            "metric_name": "test_metric_1",
+            "metric_value": "70",
+            "metric_tags": ["test_tags"],
+        },
+        {
+            "timestamp": str(fixed_timestamp),
+            "metric_name": "test_metric_2",
+            "metric_value": "5.0",
+            "metric_tags": ["test_tags"],
+        },
+    ]
+
+    mocker.patch.object(
+        Country,
+        "get_location",
+        return_value=Country(name="fr", co2g_kwh=50.0),
+    )
+    memory_metric = Metric(
+        name="test_metric_1",
+        value=HardwareInfo.get_memory_usage,
+        tags=["test_tags"],
+    )
+    cpu_metric = Metric(
+        name="test_metric_2",
+        value=HardwareInfo.get_cpu_usage,
+        tags=["test_tags"],
+    )
+
+    metrics = [memory_metric, cpu_metric]
+    exporter = JSONExporter(quit=True, metrics=metrics, path=str(test_json_file))
+    exporter.start(interval_in_seconds=interval_in_seconds)
+    exporter.stop()
+
+    exporter.start(interval_in_seconds=interval_in_seconds)
+    exporter.stop()
+    exporter.flush()
+
+    with open(test_json_file, "r") as file:
+        assert ujson.load(file) == expected
--- tracarbon/exporters/__init__.py
+++ tracarbon/exporters/__init__.py
@@ -1,3 +1,4 @@
 from tracarbon.exporters.datadog_exporter import *
 from tracarbon.exporters.exporter import *
+from tracarbon.exporters.json_exporter import *
 from tracarbon.exporters.stdout import *
